042075df5a290c0728acf9830a925166771816c4
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-16 17:24:48 +0530

    "Commit :112"

--- Commit_SameCommit_2023_10_16_17.java
+++ Commit_SameCommit_2023_10_16_17.java
@@ -1,10 +1,10 @@
-public class Commit111 {
+public class Commit112 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 111");
+			System.out.println("Exception 112");
 		}
 	}
 }
