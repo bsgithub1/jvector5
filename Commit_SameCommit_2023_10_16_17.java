a34bf6c77b593ce8ef1756ba1c7b7f71532c0e21
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-16 17:24:48 +0530

    "Commit :131"

--- Commit_SameCommit_2023_10_16_17.java
+++ Commit_SameCommit_2023_10_16_17.java
@@ -1,10 +1,10 @@
-public class Commit130 {
+public class Commit131 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 130");
+			System.out.println("Exception 131");
 		}
 	}
 }
