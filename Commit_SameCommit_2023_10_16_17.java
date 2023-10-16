e3c5ad6411ba3c4c2e7f37c2888deda2552af0f8
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-16 17:24:52 +0530

    "Commit :230"

--- Commit_SameCommit_2023_10_16_17.java
+++ Commit_SameCommit_2023_10_16_17.java
@@ -1,10 +1,10 @@
-public class Commit229 {
+public class Commit230 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 229");
+			System.out.println("Exception 230");
 		}
 	}
 }
