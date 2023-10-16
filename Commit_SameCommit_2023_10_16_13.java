a98cd661965f6fa8dd5ae6164d3fe705d05a2962
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-16 13:59:25 +0530

    "Commit :200"

--- Commit_SameCommit_2023_10_16_13.java
+++ Commit_SameCommit_2023_10_16_13.java
@@ -1,10 +1,10 @@
-public class Commit199 {
+public class Commit200 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 199");
+			System.out.println("Exception 200");
 		}
 	}
 }
