566a4800625344cf064252c06a4e7685668ae147
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-16 17:24:53 +0530

    "Commit :260"

--- Commit_SameCommit_2023_10_16_17.java
+++ Commit_SameCommit_2023_10_16_17.java
@@ -1,10 +1,10 @@
-public class Commit259 {
+public class Commit260 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 259");
+			System.out.println("Exception 260");
 		}
 	}
 }
