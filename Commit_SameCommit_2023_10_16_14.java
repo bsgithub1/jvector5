5f63aad03124a00009b71e425f0c0e2fb79a16cd
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-16 14:42:05 +0530

    "Commit :300"

--- Commit_SameCommit_2023_10_16_14.java
+++ Commit_SameCommit_2023_10_16_14.java
@@ -1,10 +1,10 @@
-public class Commit299 {
+public class Commit300 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 299");
+			System.out.println("Exception 300");
 		}
 	}
 }
