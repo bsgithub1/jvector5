d231054ac9b00f31ec5d1afd6d801201b0baa8b8
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-16 14:42:01 +0530

    "Commit :179"

--- Commit_SameCommit_2023_10_16_14.java
+++ Commit_SameCommit_2023_10_16_14.java
@@ -1,10 +1,10 @@
-public class Commit178 {
+public class Commit179 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 178");
+			System.out.println("Exception 179");
 		}
 	}
 }
