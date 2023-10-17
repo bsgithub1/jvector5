5aa5a15e84127e2b1cb7165366c299a72ec44d25
saurabh.patil <saurabh.pa@browserstack.com>
2023-10-17 17:36:34 +0530

    "Commit :129"

--- Commit_SameCommit_2023_10_17_17.java
+++ Commit_SameCommit_2023_10_17_17.java
@@ -1,10 +1,10 @@
-public class Commit128 {
+public class Commit129 {
 
 	public static void main(String args[]) {
 		try {
 			System.out.println("Screenshot taken for test : ");
 		} catch (Exception e) {
-			System.out.println("Exception 128");
+			System.out.println("Exception 129");
 		}
 	}
 }
